717daf19fbbf96f4a3db9a6f47e85e85d1577778
prav <kotecek3@gmail.com>
2020-06-18 23:07:33 +0200

    Update manifest.json Fix formatting in config_generator.js Add manifest-generator.js

--- config_generator.js
+++ config_generator.js
@@ -1,33 +1,33 @@
-"use strict"
+'use strict'
 
 const DefaultSettings = {
-blacklist: {},
-delay: 150,
-enabled: true
+  blacklist: {},
+  delay: 150,
+  enabled: true
 }
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-	if (from_ver === undefined) {
-		return Object.assign(Object.assign({}, DefaultSettings), settings)
-	} else if (from_ver === null) {
-		return DefaultSettings
-	} else {
-		if (from_ver + 1 < to_ver) {
-			settings = MigrateSettings(from_ver, from_ver + 1, settings)
-			return MigrateSettings(from_ver + 1, to_ver, settings)
-		}
-		switch (to_ver) {
-			default:
-				let oldsettings = settings
+  if (from_ver === undefined) {
+    return Object.assign(Object.assign({}, DefaultSettings), settings)
+  } else if (from_ver === null) {
+    return DefaultSettings
+  } else {
+    if (from_ver + 1 < to_ver) {
+      settings = MigrateSettings(from_ver, from_ver + 1, settings)
+      return MigrateSettings(from_ver + 1, to_ver, settings)
+    }
+    switch (to_ver) {
+      default:
+        let oldsettings = settings
 
-				settings = Object.assign(DefaultSettings, {})
+        settings = Object.assign(DefaultSettings, {})
 
-				for (let option in oldsettings) {
-					if (settings[option]) { settings[option] = oldsettings[option] }
-				}
-				break;
-		}
+        for (let option in oldsettings) {
+          if (settings[option]) { settings[option] = oldsettings[option] }
+        }
+        break
+    }
 
-		return settings
-	}
+    return settings
+  }
 }
